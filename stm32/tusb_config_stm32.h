eb89158ac2b7f512eecd388f8d334f2529398048
code-fiasco <jonathan_wilson1@outlook.com>
2026-01-25 09:33:46 +0000

    Add files via upload

---
+++ stm32/tusb_config_stm32.h
@@ -0,0 +1,41 @@
+#ifndef TUSB_CONFIG_STM32_H_
+#define TUSB_CONFIG_STM32_H_
+
+// USB Port
+#define CFG_TUSB_RHPORT0_MODE  OPT_MODE_DEVICE
+#define CFG_TUSB_RHPORT0_SPEED OPT_FULL_SPEED
+#define CFG_TUSB_RHPORT1_MODE  OPT_MODE_NONE
+
+// MCU / OS
+#define CFG_TUSB_MCU OPT_MCU_STM32F4
+#define CFG_TUSB_OS  OPT_OS_NONE
+
+// Debug
+#ifndef CFG_TUSB_DEBUG
+#define CFG_TUSB_DEBUG 0
+#endif
+
+#define CFG_TUSB_MEM_SECTION
+#define CFG_TUSB_MEM_ALIGN __attribute__((aligned(4)))
+
+// Device stack
+#define CFG_TUD_ENABLED       1
+#define CFG_TUD_ENDPOINT0_SIZE 64
+
+// Classes
+#define CFG_TUD_CDC     1
+#define CFG_TUD_MSC     0
+#define CFG_TUD_HID     1
+#define CFG_TUD_MIDI    1
+#define CFG_TUD_VENDOR  0
+
+// Buffer sizes
+#define CFG_TUD_CDC_RX_BUFSIZE  64
+#define CFG_TUD_CDC_TX_BUFSIZE  64
+#define CFG_TUD_HID_EP_BUFSIZE  64
+#define CFG_TUD_MIDI_RX_BUFSIZE 128
+#define CFG_TUD_MIDI_TX_BUFSIZE 128
+#define CFG_TUD_VENDOR_RX_BUFSIZE 64
+#define CFG_TUD_VENDOR_TX_BUFSIZE 64
+
+#endif
